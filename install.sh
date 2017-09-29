034886679b60e927db26e12428aee5062a1ab1b4
Serhii Koba <desgnkiss@gmail.com>
2017-09-29 17:08:11 +0000

    fix install script

--- install.sh
+++ install.sh
@@ -5,15 +5,15 @@ green=`tput setaf 2`
 reset=`tput sgr0`
 
 echo "${green}Cloning the project...${reset}"
-apt-get -y install git
+sudo apt-get -y install git
 git clone https://github.com/sergey-koba-mobidev/sentinel.git
 cd sentinel
 
-printf "${green}Installing packages...${reset}"
-apt-get -y install libjpeg-dev
+echo "${green}Installing packages...${reset}"
+sudo apt-get -y install libjpeg-dev
 pip install boto3
 pip install pyyaml
 pip install Pillow
 
-printf "${green}Create default config...${reset}"
+echo "${green}Create default config...${reset}"
 touch config.yml
